a6df2e0c355cbf6ce7911fd4438be9474a672e12
tonyy kosseigyy <tony kosseify>
2021-08-03 12:30:57 +0300

    Form input finished

--- src/Components/mainComponents.js
+++ src/Components/mainComponents.js
@@ -1,23 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { NetflixButton } from "./components";
-import TextField from '@material-ui/core/TextField' ;
-import './mainComponents.css' ;
+import TextField from "@material-ui/core/TextField";
+import "./mainComponents.css";
+import Fade from "react-reveal/Fade";
 
 export const MainPage = () => {
+  const [focus, setFocus] = useState(false);
+  const [input, setInput] = useState("");
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    if ( input.trim() ) {
+      const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      setError(!emailRegex.test(input));
+    } else {
+      setError(false) ;
+    }
+
+  }, [input]);
+  console.log(error);
   return (
     <MainPageContainer>
       <h1>Unlimited movies, TV shows, and more.</h1>
       <h2>Watch anywhere. Cancel anytime.</h2>
-      <p>Ready to watch? Enter your email to create or restart your membership.</p>
-      <form className='main-page-form' onSubmit={(e) => e.preventDefault() } >
-        <input autoFocus required id='email' placeholder='Email Address' type='email' />
-        <NetflixButton>Get Started</NetflixButton>
-      </form>
-
+      <p>
+        Ready to watch? Enter your email to create or restart your membership.
+      </p>
+      <div>
+        <form className="main-page-form" onSubmit={(e) => e.preventDefault()}>
+          <label
+            className={focus || input.trim() ? "focused" : ""}
+            htmlFor="email"
+          >
+            Email Address
+          </label>
+          <input
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            required
+            id="email"
+            type="email"
+            name="email"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <span className={focus ? "focused" : ""}></span>
+          <NetflixButton>Get Started</NetflixButton>
+        </form>
+        <Fade when={error} left>
+          <div>
+            <FormErrorMessage>Please enter a valid email address</FormErrorMessage>
+          </div>
+        </Fade>
+      </div>
     </MainPageContainer>
   );
 };
+
+const FormErrorMessage = styled.p`
+  color: #efa00a;
+  margin-top: 4px;
+  font-size: 17px;
+  font-weight: 400;
+  text-align: left;
+  align-self: flex-start;
+`;
 
 const MainPageContainer = styled.section`
   height: calc(100vh - 120px);
@@ -29,21 +76,22 @@ const MainPageContainer = styled.section`
   position: relative;
   color: white;
   text-align: center;
-  & > form {
-    display: flex ;
-    min-width: 50vw ;
-    margin: 0 auto ;
+  & form {
+    display: flex;
+    min-width: 50vw;
+    margin: 0 auto;
+    position: relative;
   }
-  & > form > input {
-    background-color: white ;
-    flex: 100 ;
-    outline: none ;
-    border: none ;
-    height: 80px ;
-    padding: 20px ;
-    font-size: 1.2rem ;
-    font-weight: 700 ;
-    position: relative ;
+  & form > input {
+    background-color: white;
+    flex: 100;
+    outline: none;
+    border: none;
+    height: 80px;
+    padding: 20px;
+    font-size: 1.2rem;
+    font-weight: 700;
+    position: relative;
   }
   & > h1 {
     font-size: 3.7rem;
@@ -54,20 +102,24 @@ const MainPageContainer = styled.section`
   & > h2 {
     font-size: 2rem;
     font-weight: 400;
-    margin: 20px 0;
+    margin: 15px 0;
   }
   & > p {
-    font-size: 1.2rem ;
-    font-weight: bold ;
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 15px;
   }
-  @media screen and (max-width: 1000px) {
+  @media screen and (max-width: 1200px) {
     & > h1 {
-      max-width: 100vw;
+      max-width: 95vw;
       font-size: 2.8rem;
       line-height: 3rem;
     }
     & > h2 {
       font-size: 1.5rem;
+    }
+    & > p {
+      font-size: 1rem;
     }
   }
   @media screen and (max-width: 400px) {
