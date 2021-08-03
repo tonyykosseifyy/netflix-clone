a0f4aff6aad7da162e686ce13838d4c008a9cfd5
tonyy kosseigyy <tony kosseify>
2021-08-03 18:23:30 +0300

    Netflix Section not responsive yet

--- src/Components/NetflixSection.js
+++ src/Components/NetflixSection.js
@@ -3,21 +3,30 @@ import "./mainComponents.css";
 import { data } from "./data";
 import { NetflixContainer } from "./components";
 
+
+const marginFunction = (index) => {
+    if (index % 2 === 0 ) {
+      return {marginRight: '30px'}
+    } else {
+      return { marginLeft: '30px'}
+    }
+}
+
 const NetflixSection = () => {
   return (
     <div className="netflix-containers">
       {data.map((item, index) => (
         <NetflixContainer
           key={index}
-          className="netflix-container"
+          className={`netflix-container netflix-container-${index}`}
           number={index}
         >
-          <div className="neflix-container-data">
+          <div className="neflix-container-data" style={marginFunction(index)}>
             <h1>{item.title}</h1>
             <h2>{item.description}</h2>
           </div>
           <div className="netflix-container-animations">
-            <img src={item.image} alt={item.title} />
+            <img src={item.image} alt={item.title} style={{zIndex: '2', position: 'relative'}} />
             { item.hasOwnProperty('srcs') ?
               <div className='netflix-srcs'>
                 <img src={item.srcs[0]} alt={item.title} />
--- src/Components/data.js
+++ src/Components/data.js
@@ -5,7 +5,8 @@ export const data = [
       "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
     image:
       "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png",
-    src: 'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v'
+    src:
+      "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v",
   },
   {
     title: "Download your shows to watch offline.",
@@ -13,21 +14,24 @@ export const data = [
       "Save your favorites easily and always have something to watch.",
     image:
       "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg",
-    srcs : [
-      'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png' ,
-    ]
+    srcs: [
+      "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png",
+    ],
   },
   {
     title: "Watch everywhere.",
     description:
       "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.",
     image:
-      'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png',
+      "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png",
+    src:
+      "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v",
   },
   {
     title: "Create profiles for kids.",
     description:
       "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
-    image: 'https://occ-0-3312-778.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABdFTpLmANuJpYneLq8L5m7CunMCi8e8Nl4y7xaPVWzG3IeoDoq17egTQAthApKg_4sdRWdwuR8KadWu1frjL3JQImpwq.png?r=fcd'
+    image:
+      "https://occ-0-3312-778.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABdFTpLmANuJpYneLq8L5m7CunMCi8e8Nl4y7xaPVWzG3IeoDoq17egTQAthApKg_4sdRWdwuR8KadWu1frjL3JQImpwq.png?r=fcd",
   },
 ];
