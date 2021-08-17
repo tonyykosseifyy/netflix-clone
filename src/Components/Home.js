d3b6fdb10a4bf40a19a9edfec056b7a85e6b3005
tonyykosseifyy <tonykosseify123@gmail.com>
2021-08-17 21:45:03 +0300

    working on the homePage navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react' ;
 import Navbar from './Navbar' ;
 import './Home.css' ;
+import { useLocation } from "react-router-dom";
+
+
 
 
 const Home = () => {
+
   return (
     <div className='home'>
       <Navbar home={true} />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import styled from "styled-components";
 import { NetflixLogo, NetflixButton } from "./components.js";
 import { useLocation , Link } from "react-router-dom";
-
+import { useSelector } from 'react-redux' ;
 
 {
   /* let location = useLocation() ;
@@ -11,20 +11,44 @@ import { useLocation , Link } from "react-router-dom";
 */
 }
 //ctrl-alt-f
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
-const Navbar = () => {
+const checkIfActive = (query , value ) => {
+  if( query.get("search") === value ) {
+    return "active"
+  } else return ""
+}
+
+const Navbar = ({ home }) => {
+  let query = useQuery() ;
   let location = useLocation();
+  const signedIn = useSelector(state => state.user.signedIn)
 
   return (
-    <NavContainer className="navbar">
-      <NetflixLogo />
-      <Link to='/login' className='no-styling-link'>
+    <NavContainer className="navbar" home={home}>
+      <Link to="/" className='no-styling-link' >
+        <NetflixLogo />
+      </Link>
+
+      <Link to='/login' className='no-styling-link' style={{display: home ? "none" : ""}}>
         <NetflixButton
           style={{ display: location?.pathname === "/login" ? "none" : "" }}
         >
           Sign Up
         </NetflixButton>
       </Link>
+
+      <div className="navbar-link-container" style={{display: signedIn ? "" : "none"}}>
+        <Link to="/">Home</Link>
+        <Link to="/browse?search=tv-shows">TV Shows</Link>
+        <Link to="/browse?search=movies">Movies</Link>
+        <Link to="/browse?search=recently-added">Recently Added</Link>
+        <Link to="/browse?search=my-list">My List</Link>
+      </div>
+
+
     </NavContainer>
   );
 };
@@ -35,15 +59,7 @@ const NavContainer = styled.nav`
   height: 120px;
   padding: 25px 4.3vw;
   display: flex;
-  justify-content: space-between;
+  justify-content: ${props => props.home ? "" : "space-between"} ;
   align-items: center;
   position: relative;
-  & > svg {
-    width: 10vw;
-    height: 50px;
-    object-fit: contain;
-    fill: var(--netflix-red);
-    max-width: 180px;
-    min-width: 100px;
-  }
 `;
