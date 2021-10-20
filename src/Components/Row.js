ad0f006611c09f7992306d5b188499f196ccd10f
tonyykosseifyy <tonykosseify123@gmail.com>
2021-10-20 20:49:01 +0300

    installing swiper for rows navigation

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -2,7 +2,13 @@ import { useState , useEffect } from "react" ;
 import "./Row.css" ;
 import axios from "../axios" ;
 import { base_url } from "../requests" ;
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from 'swiper';
 
+// Import Swiper styles
+//npm install swiper@6.8.4
+import "swiper/css/bundle";
+SwiperCore.use([Pagination]);
 
 //movie.poster_path : movie.backdrop_path
 const Row = ({ title , url }) => {
@@ -32,3 +38,9 @@ const Row = ({ title , url }) => {
 };
 
 export default Row ;
+
+/*<Swiper slidesPerView={3} spaceBetween={30} freeMode={true} pagination={{
+  "clickable": true
+}} className="mySwiper">
+  <SwiperSlide>Slide 1</SwiperSlide><SwiperSlide>Slide 2</SwiperSlide><SwiperSlide>Slide 3</SwiperSlide><SwiperSlide>Slide 4</SwiperSlide><SwiperSlide>Slide 5</SwiperSlide><SwiperSlide>Slide 6</SwiperSlide><SwiperSlide>Slide 7</SwiperSlide><SwiperSlide>Slide 8</SwiperSlide><SwiperSlide>Slide 9</SwiperSlide>
+  </Swiper>*/
