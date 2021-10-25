632adb8286220f9dc5006fbf7e3a0d438471b4e5
tonyykosseifyy <tonykosseify123@gmail.com>
2021-10-25 19:45:38 +0300

    styling the h1

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
 import axios from "../axios" ;
 import { API_KEY , base_url } from "../requests";
+import { CompareArrowsOutlined } from "@material-ui/icons";
 
 const useQuery = () => new URLSearchParams(useLocation().search);
 
@@ -12,8 +13,9 @@ const array = ["my-list" , "movies" , "recently-added" , "tv-shows"];
 
 const requests = {
     fetchTvShows: `https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}&language=en-US`, 
-
-}
+    fetchTrending : `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
+};
 
 const SearchResults = () => {
     let query = useQuery() ;
@@ -34,26 +36,34 @@ const SearchResults = () => {
             setSearchResults([]) ;
         }
     }
+    const fetchMovies = async( url ) => {
+        try {
+            const response = await axios.get(url)
+            setSearchResults(response.data.results) ;
+            console.log(response);
+        } catch(err) {setSearchResults([])}
+    }
     useEffect(() => {
         if ( array.some(( item ) => item === search )) {
-
+            search === "tv-shows" ? fetchMovies(requests.fetchTvShows):search === "recently-added"? fetchMovies(requests.fetchTrending) : search === "movies" && fetchMovies(requests.fetchTopRated) ;
         } else SearchMovie() ;
     }, [ search ])
     console.log(searchResults) ;
     return (
         <SearchWrapper className='home'>
             <Navbar home inSearch searchValue={search} handleInputSearch={setSearch} />
+            { searchResults.length > 0 && <SearchHeader>Results For " <strong>{search.replace("-"," ")}</strong> "</SearchHeader>}
             <SearchContainer>
                 { searchResults.length > 0 ? searchResults.map((movie , index) => (
                     <img 
                         key={movie.id || index } 
-                        src={`${base_url}${movie.backdrop_path ? movie.backdrop_path:movie.poster_path}`} 
+                        src={`${base_url}${movie.backdrop_path}`} 
                         alt={movie.name} 
-                        style={{display : !movie.backdrop_path && !movie.poster_path && "none"}}
+                        style={{display : !movie.backdrop_path && "none"}}
                     />
                 )) 
                 : 
-                <p>No Results Found for <strong>{search}</strong></p>}
+                <p style={{fontWeight:"300"}}>No Results Found for <strong>{search.replace("-"," ")}</strong></p>}
             </SearchContainer>
         </SearchWrapper>
     );
@@ -68,15 +78,13 @@ const SearchWrapper = styled.main``
 
 const SearchContainer = styled.section`
     margin: 0 auto ;
-    margin-top: 30px;
-    padding: 30px 20px;
+    padding: 20px;
     display: grid ;
     grid-template-columns: repeat(4 , 1fr );
-    grid-template-rows: repeat(3 , minmax(60px , 100px));
     grid-column-gap: 5px ;
     grid-row-gap: 70px ;
     width: 100% ;
-    max-width: 1300px ;
+    max-width: 1200px ;
     @media (max-width: 800px) {
         grid-template-columns: repeat(3 , 1fr);
         grid-row-gap: 60px ;
@@ -89,5 +97,18 @@ const SearchContainer = styled.section`
         height: 100% ;
         width: 100% ;
         object-fit: cover;
+        transition: .3s ease-out;
+        cursor: pointer;
     }
+    & > img:hover {
+        transform: scale(1.07);
+    }
+`
+
+const SearchHeader = styled.h1`
+    font-weight: 300;
+    font-size: 1rem;
+    margin-top: 50px;
+    margin-left: 20px;
+    text-transform: capitalize;
 `
