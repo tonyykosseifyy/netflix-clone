acea4d83b4cf0b47671d0ffa74f13b398c37563c
tonyykosseifyy <tonykosseify123@gmail.com>
2021-10-25 13:26:23 +0300

    search navbar container

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useRef } from "react";
 import "./Navbar.css";
 import styled from "styled-components";
 import { NetflixLogo, NetflixButton } from "./components.js";
@@ -6,12 +6,11 @@ import { useLocation , Link } from "react-router-dom";
 import { useSelector } from 'react-redux' ;
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-{
-  /* let location = useLocation() ;
-    console.log(location) ;
-*/
-}
+import SearchIcon from '@material-ui/icons/Search';
+
 //ctrl-alt-f
+
+const netflixFavicon = "https://occ-0-3312-778.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABUCZYfPbupvQjzSa3egePk8TFNDy2A_w15DEAq50IqW8MYmOtmbWwN4Txem7mgNYEMPJ1BY6uasiIJQ8JeFO3EU.png?r=b97" ;
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -22,8 +21,12 @@ const Navbar = ({ home }) => {
   let query = useQuery() ;
   let location = useLocation();
   const signedIn = useSelector(state => state.user.signedIn)
+  const photoURL = useSelector(state => state.user.photoURL);
   const [ open , setOpen ] = useState(false) ;
   const [ scroll , setScroll ] = useState(false) ;
+  const [ search , setSearch ] = useState("") ;
+  const [ openSearch , setOpenSearch ] = useState(false) ;
+  const searchRef = useRef(null);
   window.addEventListener("scroll" , () => setScroll(window.scrollY)) ;
 
   return (
@@ -66,6 +69,34 @@ const Navbar = ({ home }) => {
         <Link onClick={() => setOpen(false)} className={checkIfActive(query,"my-list")} to="/browse?search=my-list">My List</Link>
       </div>
 
+      { signedIn &&
+        <RightNavbar>
+          <SearchContainer open={openSearch} > 
+          <SearchIcon onClick={() => setOpenSearch(!openSearch)} />
+            <input 
+            ref={searchRef}
+            type="text" 
+            placeholder="Search movies..." 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)} 
+            onBlur={() => setOpenSearch(false)}
+            />
+            
+          </SearchContainer>
+
+          <AccountNavbar>
+            <img src={photoURL ? photoURL : netflixFavicon } alt="User Image" />
+            <ArrowDropDownIcon />
+            <div>
+              <ArrowDropUpIcon />
+              <a>Accout</a>
+              <a>Help Center</a>
+              <a>Sign out of Netflix</a>
+            </div>
+            
+          </AccountNavbar>
+        </RightNavbar>
+        }
     </NavContainer>
   );
 };
@@ -103,5 +134,97 @@ const BrowseContainer = styled.div`
   }
   @media (max-width:800px) {
     display: flex;
+  }
+`
+const RightNavbar = styled.div`
+  margin-left: auto ;
+  display: flex ;
+  margin-top: -8px;
+`
+
+const SearchContainer = styled.div`
+  position: relative;
+  width: ${props => props.open ? "250px" : "1.1em"} ;
+  background-color: ${props => props.open ? "#0C0C0C" : "transparent"};
+  border: ${props => props.open ? "1px solid white" : "none"};
+  transition: width .3s ease-out;
+  padding: 3px ;
+  display: flex ;
+  & > svg {
+    width: 1.1em ;
+    height: 1.1em;
+  }
+  & > input:placeholder {
+    font-weight: lighter ;
+  }
+  & > input {
+    margin-left: 7px;
+    outline: none ;
+    border: none;
+    color: white;
+    font-weight: 400;
+    min-width: 0;
+    transition: .3s ease-out ;
+    flex: 100 ;
+    background: transparent;
+  }  
+`
+
+const AccountNavbar = styled.div`
+  margin-left: 30px;
+  display: flex ;
+  align-items: center;
+  position: relative ;
+  cursor: pointer;
+  & > svg {
+    transition: .3s ease-out;
+  }
+  &:hover > svg {
+    transform: rotate(180deg);
+  }
+  & > img {
+    width: 1.9em ;
+    height: 1.9em;
+    object-fit: contain ;
+    border-radius: 8px;
+  }
+  & > div {
+    position: absolute;
+    top: 50px;
+    background-color: rgb(0 0 0 / 80%);
+    color: white;
+    min-width: 140px;
+    font-size: 14px;
+    left: -50px;
+    padding: 10px 5px;
+    transform: scaleY(0) ;
+    transform-origin: top;
+    transition: transform .2s ease-out;
+    overflow: hidden;
+    display: flex ;
+    flex-direction: column ;
+    transition-delay: .3s ;
+  }
+  & > div > a {
+    color: white;
+    text-decoration: none ;
+    text-decoration-color: white ;
+    cursor: pointer;
+    font-size: 14px;
+    padding: 5px 5px ;
+  }
+  & > div > a:hover {
+    text-decoration: underline;
+  }
+  & > div > svg {
+    position: absolute;
+    top: -21px;
+    font-size: 1.8rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &:hover > div {
+    border: 1px solid #444445;
+    transform: scaleY(1);
   }
 `
